29af7c06726420f2f8952a2b0b0e883a4d160a0d
davidalpha <david.alpha@gmail.com>
2024-04-22 16:21:35 +0700

    less aircontrol

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,7 +16,11 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		velocity.x = velocity.x/2
+		var input_dir = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED/2
+			velocity.z = direction.z * SPEED/2
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
@@ -24,14 +28,15 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("player_left", "player_right", "player_up", "player_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if is_on_floor():
+		var input_dir = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 
